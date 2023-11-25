51633e68958b8d02a343147af15c272990495221
martin <tincho8919@gmail.com>
2023-11-25 11:42:00 -0300

    url

--- database/db.js
+++ database/db.js
@@ -4,16 +4,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 
-/* const PORT = process.env.PORT || 8080
-const DB_NAME = process.env.DB_NAME
-const DB_PASSWORD = process.env.DB_PASSWORD
-const URL = process.env.URL */
+const PORT = process.env.PORT || 8080
+/* const DB_NAME = process.env.DB_NAME
+const DB_PASSWORD = process.env.DB_PASSWORD */
+const URL = process.env.URL 
 
 
 // Configura la opción strictQuery
 mongoose.set('strictQuery', false);
 
-const db = mongoose.connect('mongodb+srv://tincho8919:34697923@backendutn.2pwp6mp.mongodb.net/backendutn?retryWrites=true&w=majority',  {
+const db = mongoose.connect(URL,  {
     useNewUrlParser: true,
 })
 .then(() =>{
